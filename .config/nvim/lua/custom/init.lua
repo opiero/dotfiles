8fefb88b6f7a5fc1e0e23575805ad6a1d0c061be
Piero Capelo <piero.capelo@proton.me>
2024-03-16 00:31:29 -0300

    updated dependencies

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -2,3 +2,4 @@ vim.wo.relativenumber = true --setting relative line number
 vim.wo.number = true --removing line number
 vim.wo.linebreak = true --breaking lines
 vim.opt.swapfile = false --no swap file
+vim.g.python3_host_prog = '/usr/bin/python3'
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,101 +1,144 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 local plugins = {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
-	{
-		"ThePrimeagen/vim-be-good",
-		event = "VeryLazy",
-	},
-	{
-		"gptlang/CopilotChat.nvim",
-		event = "VeryLazy",
-	},
-	{
-		"mfussenegger/nvim-lint",
-		event = "VeryLazy",
-		config = function()
-			require("custom.configs.lint")
-		end,
-	},
-	{
-		"mhartington/formatter.nvim",
-		event = "VeryLazy",
-		opts = function()
-			return require("custom.configs.formatter")
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"black",
-				"eslint_d",
-				"mypy",
-				"luacheck",
-				"prettier",
-				"stylua",
-				"pyright",
-				"ruff",
-				"lua-language-server",
-				"isort",
-				"typescript-language-server",
-			},
-		},
-	},
-	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		opts = overrides.copilot,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "ThePrimeagen/vim-be-good",
+    event = "VeryLazy",
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "custom.configs.lint"
+    end,
+  },
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.formatter"
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "eslint_d",
+        "mypy",
+        "luacheck",
+        "prettier",
+        "stylua",
+        "pyright",
+        "ruff",
+        "lua-language-server",
+        "isort",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require "nvim-treesitter.configs"
 
-			configs.setup({
-				ensure_installed = {
-					"python",
-					"lua",
-					"typescript",
-					"javascript",
-					"json",
-					"yaml",
-					"bash",
-					"sql",
-					"vimdoc",
-          "go"
-				},
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
-	{
-		"Equilibris/nx.nvim",
+      configs.setup {
+        ensure_installed = {
+          "python",
+          "lua",
+          "typescript",
+          "javascript",
+          "json",
+          "yaml",
+          "bash",
+          "sql",
+          "vimdoc",
+          "go",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end,
+  },
+  {
+    "Equilibris/nx.nvim",
 
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
 
-		opts = {
-			nx_cmd_root = "nx",
-		},
-		event = "VeryLazy",
-		keys = {
-			{ "<leader>nx", "<cmd>Telescope nx actions<CR>", desc = "nx actions" },
-		},
-	},
+    opts = {
+      nx_cmd_root = "nx",
+    },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>nx", "<cmd>Telescope nx actions<CR>", desc = "nx actions" },
+    },
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
+      -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
+      -- temperature = 0.1,
+    },
+    build = function()
+      vim.notify "Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim."
+    end,
+    event = "VeryLazy",
+    keys = {
+      { "<leader>ccb", ":CopilotChatBuffer ", desc = "CopilotChat - Chat with current buffer" },
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      {
+        "<leader>ccT",
+        "<cmd>CopilotChatVsplitToggle<cr>",
+        desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
+      },
+      {
+        "<leader>ccv",
+        ":CopilotChatVisual ",
+        mode = "x",
+        desc = "CopilotChat - Open in vertical split",
+      },
+      {
+        "<leader>ccx",
+        ":CopilotChatInPlace<cr>",
+        mode = "x",
+        desc = "CopilotChat - Run in-place code",
+      },
+      {
+        "<leader>ccf",
+        "<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
+        desc = "CopilotChat - Fix diagnostic",
+      },
+      {
+        "<leader>ccr",
+        "<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
+        desc = "CopilotChat - Reset chat history and clear buffer",
+      },
+    },
+  },
 }
 return plugins
