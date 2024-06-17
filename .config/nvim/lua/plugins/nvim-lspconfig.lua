8a6c1cb1eaf29a79bd235711f279dabfd4116573
Piero Capelo <piero.capelo@proton.me>
2024-06-17 10:34:14 -0300

    added ruff as linter

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -108,28 +108,16 @@ local M = { -- LSP Configuration & Plugins
     local servers = {
       pyright = {},
       gopls = {},
+      ruff = {},
       clangd = {},
       arduino_language_server = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
       tsserver = {},
-      --
-
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes { ...},
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
       },
