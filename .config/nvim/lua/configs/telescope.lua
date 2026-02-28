e14e8dca74a00c99c2a7a5b6d17fe10214c8147f
Piero Capelo <piero.capelo@proton.me>
2026-02-28 13:19:41 -0300

    added kitty, set it as main terminal and now telescope shows hidden files

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -1,4 +1,9 @@
 require('telescope').setup {
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
   extensions = {
     ['ui-select'] = {
       require('telescope.themes').get_dropdown(),
